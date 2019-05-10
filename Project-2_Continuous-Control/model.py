68d58ff129a2316f32bbb1692bfd8fbada367ac4
Rafael1s <klivlend1@yahoo.com>
2019-05-10 09:23:12 +0300

    Update model.py

--- Project-2_Continuous-Control/model.py
+++ Project-2_Continuous-Control/model.py
@@ -44,7 +44,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
