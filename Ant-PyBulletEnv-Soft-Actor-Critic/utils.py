3cad56585e82f36adfd3da316e3ccc89a258d591
Rafael1s <klivlend1@yahoo.com>
2020-04-29 13:21:00 +0300

    Add TwinDelayed_U.py

---
+++ Ant-PyBulletEnv-Soft-Actor-Critic/utils.py
@@ -0,0 +1,7 @@
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
