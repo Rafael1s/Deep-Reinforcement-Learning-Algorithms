f0f6b2f8bcaa9edb3f2ed0f5c1e1a449d3f06e64
Rafael1s <klivlend1@yahoo.com>
2020-10-18 21:40:42 +0300

    Add Minitaur-SAC_lr0.0001-b128-1745ep.ipynb

---
+++ Minitaur-Soft-Actor-Critic/replay_memory.py
@@ -0,0 +1,29 @@
+import random
+import numpy as np
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        ''' 
+        https://github.com/quantumiracle/SOTA-RL-Algorithms/blob/master/sac_v2_multiprocess.py
+        the * serves as unpack: sum(a,b) <=> batch=(a,b), sum(*batch) ;
+        zip: a=[1,2], b=[2,3], zip(a,b) => [(1, 2), (2, 3)] ;
+        the map serves as mapping the function on each list element: map(square, [2,3]) => [4,9] ;
+        np.stack((1,2)) => array([1, 2])
+        '''
+        return state, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
